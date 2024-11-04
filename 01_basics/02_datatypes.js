ade26aa3101c6a996c81b0249be8d2549c79f072
NareshChary0430 <nareshchari300@gmail.com>
2024-11-04 18:42:19 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//alert() we are using nodejs not server side
+
+//console.log(3
+    +3// code should be readability high
+//);
+
+console.log("Naresh");//this is perfer
+
+let name = "Naresh";
+let age = 18;
+let isLoggedin = false;
+let state = null;
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true or false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => unique
+//object 
+
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
