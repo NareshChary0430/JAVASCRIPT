677be1d98896d080e5793be6d52f46d4970994c8
NareshChary0430 <nareshchari300@gmail.com>
2024-11-04 18:56:10 +0000

    container config done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => Nan
+//true => 1
+//false => 0
+//null => 0
+//undefined => Nan
+
+let isLoggedin = 1;
+let booleanIsLoggedIn = Boolean(isLoggedin);
+// console.log(booleanIsLoggedIn);
+
+// if we convert 1 value to boolean it will give true
+// if we convert 0 value to boolean it will give false
+// if it is empty it will give false
+//"Naresh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
