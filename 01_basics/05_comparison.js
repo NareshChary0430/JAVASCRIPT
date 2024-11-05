f8773e63c85be972a025c0874896ff5132534147
NareshChary0430 <nareshchari300@gmail.com>
2024-11-05 17:28:15 +0000

    container config done

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,24 @@
+
+// this are normal comparisons
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+
+console.log("2" > 1); // datatypes sholud be same
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check  this will chech datatypes
+
+console.log("2" === 2);
