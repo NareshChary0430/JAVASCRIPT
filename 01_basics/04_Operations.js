c045108560ca52df6172fcb5497310697debdb8d
NareshChary0430 <nareshchari300@gmail.com>
2024-11-05 13:41:03 +0000

    container config done

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,41 @@
+let Value = 3
+let negValue = -Value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+ 
+let str1 = "Hello";
+let str2 = "Naresh";
+
+let str3 = str1+str2;
+
+console.log(str3);
+
+
+console.log("1"+2);  // => 12
+console.log(1+"2");  // => 12
+console.log("1"+"2"); // => 12 
+console.log("1"+2+2); //=> 122
+console.log(1+2+"2"); //=> 32
+
+console.log(+true); // this will give output has 1
+
+// console.log(true+); // this is not allowed 
+
+console.log(+""); // this will give output has 0 
+
+//Increments Operators
+
+let gameCounter1= 100; //post
+gameCounter1++;
+console.log(gameCounter1);
+
+let gameCounter2 = 100;
+++gameCounter2; //prefix
+console.log(gameCounter2);
